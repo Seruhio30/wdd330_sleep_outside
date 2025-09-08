44f0e7c8a9a810b116e8e79233baf9485a9db778
Seruhio30 <sh0041314@gmail.com>
2025-09-08 15:11:33 -0600

    actividad de grupo realizada por Sergio Herrera

--- src/js/product.js
+++ src/js/product.js
@@ -1,26 +1,36 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import { getParam } from './utils.mjs';
+import ProductDetails from './productDetails.mjs';
 
+const productId = getParam('product');
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  let cartItems = getLocalStorage('so-cart');
+//solo para prueba
+//dataSource.findProductById(productId).then(product => {
+//console.log(product);
+//});
 
-  // Asegúrate de que sea un array
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-  cartItems.push(product);
-  setLocalStorage('so-cart', cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// function addProductToCart(product) {
+//   let cartItems = getLocalStorage('so-cart');
+//
+//   // Asegúrate de que sea un array
+//   if (!Array.isArray(cartItems)) {
+//     cartItems = [];
+//   }
+//
+//   cartItems.push(product);
+//   setLocalStorage('so-cart', cartItems);
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+//
+// // add listener to Add to Cart button
+// document
+//   .getElementById('addToCart')
+//   .addEventListener('click', addToCartHandler);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,10 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+//week two team activity (create new fuction called getParam(param))
+export function getParam(param){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
