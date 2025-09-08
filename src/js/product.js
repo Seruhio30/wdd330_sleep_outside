72bdddeecc9a8b80acc1428b1dba52583bbb8d7c
Seruhio30 <sh0041314@gmail.com>
2025-09-07 20:06:32 -0600

    Fix: carrito ahora acumula múltiples productos correctamente

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,18 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let cartItems = getLocalStorage('so-cart');
+
+  // Asegúrate de que sea un array
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
+
+  cartItems.push(product);
+  setLocalStorage('so-cart', cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
